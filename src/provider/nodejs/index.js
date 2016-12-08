21def3c823eee58f10bb90eb214c313b7bb8b2bc
Roman Dvornov <rdvornov@gmail.com>
2016-12-09 02:00:01 +0300

    use default rempl port

--- src/provider/nodejs/index.js
+++ src/provider/nodejs/index.js
@@ -3,5 +3,5 @@ var Client = require('./client');
 var makeSync = require('./sync.js');
 
 module.exports = Provider.factory(function(id, getRemoteUI, endpoint) {
-    return makeSync(new Provider(id, getRemoteUI), new Client(endpoint || '//localhost:8123'));
+    return makeSync(new Provider(id, getRemoteUI), new Client(endpoint || '//localhost:8177'));
 });
