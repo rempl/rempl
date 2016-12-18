1a67b5341a823ec3bc7e76e7ab90027c7730ee58
Roman Dvornov <rdvornov@gmail.com>
2016-12-19 01:41:01 +0300

    fix default ws host for file: scheme

--- src/publisher/browser/sync-ws.js
+++ src/publisher/browser/sync-ws.js
@@ -3,7 +3,7 @@
 var Transport = require('./transport-ws.js');
 var identify = require('./identify.js');
 var meta = document.querySelector && document.querySelector('meta[name="rempl:server"]');
-var endpoint = (meta && meta.getAttribute('value')) || ('ws://' + location.hostname + ':8177');
+var endpoint = (meta && meta.getAttribute('value')) || ('ws://' + (location.hostname || 'localhost') + ':8177');
 var transport;
 
 module.exports = function(publisher, callback) {
