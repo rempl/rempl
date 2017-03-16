e2e1ebc0ef40252a66e9f5ac043493e198fd5ab8
Roman Dvornov <rdvornov@gmail.com>
2017-03-17 01:43:47 +0300

    show reload button in dev mode and non-browser env

--- server/client/src/ui/index.js
+++ server/client/src/ui/index.js
@@ -8,7 +8,7 @@ var endpoint = require('../endpoint.js');
 module.exports = new Node({
     template: resource('./template/layout.tmpl'),
     binding: {
-        /** @cut */ dev: basis.fn.$const(true),
+        /** @cut */ dev: basis.fn.$const(window !== top),
         online: transport.online,
         pickMode: endpoint.isPickMode,
         endpoints: 'satellite:',
