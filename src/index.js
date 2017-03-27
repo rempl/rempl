2cded09c7ee6f37ff998fd97088402d7f10f3b55
Roman Dvornov <rdvornov@gmail.com>
2017-03-27 22:11:30 +0300

    implement rempl.version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 module.exports = {
+    version: require('../package.json').version,
+    source: require('fs').readFileSync(__dirname + '/../dist/rempl.js', 'utf8'),
+
     createPublisher: require('./publisher/index.js'),
     createSandbox: require('./sandbox/index.js'),
     getSubscriber: require('./subscriber/index.js'),
 
-    source: require('fs').readFileSync(__dirname + '/../dist/rempl.js', 'utf8'),
     scriptFromFile: require('./sdk/scriptFromFile.js')
 };
