767323e73824c632f628458b50944b0b6e409429
Roman Dvornov <rdvornov@gmail.com>
2017-03-29 01:25:04 +0300

    fix browser extension endpoint name (subscriber -> host)

--- src/publisher/browser/sync-browser-extension.js
+++ src/publisher/browser/sync-browser-extension.js
@@ -1,3 +1,3 @@
 var EventTransport = require('../../transport/event.js');
 
-module.exports = new EventTransport('rempl-publisher', 'rempl-browser-extension-subscriber').onInit;
+module.exports = new EventTransport('rempl-publisher', 'rempl-browser-extension-host').onInit;
