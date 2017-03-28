aba3005f2897f4f1c48012f5cd3a88b93c40e731
Roman Dvornov <rdvornov@gmail.com>
2017-03-28 22:13:47 +0300

    small fixes

--- src/source.js
+++ src/source.js
@@ -1,1 +1,2 @@
+// when update this assignment please  make appropriate changes in scripts/process-rempl-bundle.js
 module.exports = require('fs').readFileSync(__dirname + '/../dist/rempl.js', 'utf8');
