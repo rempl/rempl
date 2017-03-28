d139e56b5f93ad64376d2e923a00fd5cd86d4054
Roman Dvornov <rdvornov@gmail.com>
2017-03-28 03:00:27 +0300

    basic implementation of in-page host/sandbox

--- src/publisher/browser/sync-in-page.js
+++ src/publisher/browser/sync-in-page.js
@@ -1,3 +1,31 @@
-var EventTransport = require('../../transport/event.js');
+/* eslint-env browser */
+var createSandbox = require('../../sandbox/index.js');
 
-module.exports = new EventTransport('rempl-publisher', 'rempl-in-page-subscriber').onInit;
+module.exports = function(publisher, callback) {
+    // disable it by default since it's a basic implementation
+    if (true) {
+        return;
+    }
+
+    if (typeof document === 'undefined') {
+        return;
+    }
+
+    var container = document.createElement('div');
+    container.style = 'position:fixed;z-index:100000;left:0;right:0;bottom:0;height:50%;border-top:2px solid #AAA;background:#EEE;opacity:.9';
+    container.innerHTML = '<style>iframe{position:absolute;top:0;left:0;width:100%;height:100%;border:0;background:white}</style>';
+    document.documentElement.appendChild(container);
+
+    publisher.getRemoteUI({}, function(error, type, content) {
+        createSandbox({
+            container: container,
+            type: type,
+            content: content
+        }, function(api) {
+            callback(api);
+            api.send({
+                type: 'publisher:connect'
+            });
+        });
+    });
+};
--- src/publisher/browser/sync.js
+++ src/publisher/browser/sync.js
@@ -1,6 +1,6 @@
 module.exports = function createSync(publisher) {
     var syncBrowserExtension = require('./sync-browser-extension.js');
-    // var inpageSync = require('./sync-in-page.js');
+    var inpageSync = require('./sync-in-page.js');
     var syncWs = require('./sync-ws.js');
 
     // browser extension
@@ -12,13 +12,12 @@ module.exports = function createSync(publisher) {
     });
 
     // // in page
-    // inpageSync(publisher, function(api) {
-    //     api.subscribe(publisher.processInput);
-    //     api.connected.link(function(connected) {
-    //         // devtools.set(connected);
-    //         publisher.channels.inPage = connected ? api.send : null;
-    //     });
-    // });
+    inpageSync(publisher, function(api) {
+        api.subscribe(publisher.processInput);
+        api.connected.link(function(connected) {
+            publisher.setupChannel('in-page', api.send, connected);
+        });
+    });
 
     // ws server
     syncWs(publisher, function(api) {
