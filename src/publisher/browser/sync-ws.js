23a715ae3dc29551ccbae54eb46683f4b7630dce
Roman Dvornov <rdvornov@gmail.com>
2017-03-29 22:00:19 +0300

    fix publisher ws transport

--- src/publisher/browser/sync-ws.js
+++ src/publisher/browser/sync-ws.js
@@ -4,15 +4,15 @@ var Transport = require('./transport-ws.js');
 var identify = require('./identify.js').wrapTransport;
 var meta = document.querySelector && document.querySelector('meta[name="rempl:server"]');
 var endpoint = (meta && meta.getAttribute('value')) || ('ws://' + (location.hostname || 'localhost') + ':8177');
-var api;
+var transport;
 
 module.exports = function(publisher, callback) {
-    if (!api) {
-        api = identify(new Transport(endpoint)).createApi(
-            publisher.id,
-            publisher.getRemoteUI
-        );
+    if (!transport) {
+        transport = identify(new Transport(endpoint));
     }
 
-    callback(api);
+    callback(transport.createApi(
+        publisher.id,
+        publisher.getRemoteUI
+    ));
 };
