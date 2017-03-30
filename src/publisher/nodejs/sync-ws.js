c0f509abdf05990d730d0fd731a7cff950ec6aba
Roman Dvornov <rdvornov@gmail.com>
2017-03-30 12:49:22 +0300

    publisher ws sync (nodejs) when multiple publishers is used

--- src/publisher/nodejs/sync-ws.js
+++ src/publisher/nodejs/sync-ws.js
@@ -1,14 +1,14 @@
 var Transport = require('./transport-ws.js');
 var endpoint = process.env.REMPL_SERVER || 'ws://localhost:8177';
-var api;
+var transport;
 
 module.exports = function(publisher, callback) {
-    if (!api) {
-        api = new Transport(publisher.wsendpoint || endpoint).createApi(
-            publisher.id,
-            publisher.getRemoteUI
-        );
+    if (!transport) {
+        transport = new Transport(publisher.wsendpoint || endpoint);
     }
 
-    callback(api);
+    callback(transport.createApi(
+        publisher.id,
+        publisher.getRemoteUI
+    ));
 };
