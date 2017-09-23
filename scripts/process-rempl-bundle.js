8b2dd1d9331653cc0c617164e7d19c608619d26f
Roman Dvornov <rdvornov@gmail.com>
2017-09-23 03:48:51 +0300

    fix known issues with bundle working in various environments

--- scripts/process-rempl-bundle.js
+++ scripts/process-rempl-bundle.js
@@ -1,19 +1,35 @@
-var buffer = [];
+var buffer = '';
 
 process.stdin
     .setEncoding('utf8')
     .on('data', function(chunk) {
-        buffer.push(chunk);
+        buffer += chunk;
     })
     .on('end', function() {
         process.stdout.write(
             '(function __selfScript(){' +
+            // insert global definition because it may to be defined in outer bundle (upper scopes)
+            // and after minifier quine break because of variable renaming
+            'var global = new Function(\'return"undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{}\')();' +
+            // main part
             (buffer
-                .join('')
+                // remove amd since it leads to errors when using with webpack
+                .replace(
+                    /else if\(typeof define==="function"&&define\.amd\){define\(\[\],f\)}/,
+                    ''
+                )
+                // replace getting a global in socket.io modules for explicit `global` reference,
+                // since this solution doesn't work in strict mode
+                .replace(
+                    /function\(\){return this}\(\)/g,
+                    'global'
+                )
+                // replace file read with its content
                 .replace(
                     /typeof asset(.|\s)+?require\('fs'\)\.readFileSync\(__dirname \+ '\/style\.js', 'utf8'\)/,
                     JSON.stringify(require('fs').readFileSync(__dirname + '/../src/host/in-page/style.css', 'utf8'))
                 )
+                // replace file read with its content
                 .replace(
                     /module.exports = require\('fs'\).+?(?=;)/,
                     'module.exports = "(" + __selfScript + ").call(this);"'
